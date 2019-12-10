66970d20bab4a999c12c488afb6634a1e6820342
houssam <hlaghzil@gmail.com>
2019-12-10 10:11:10 +0100

    début

--- src/main/java/fr/projet5/App.java
+++ src/main/java/fr/projet5/App.java
@@ -1,5 +1,10 @@
+package fr.projet5;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+        
     }
 }
