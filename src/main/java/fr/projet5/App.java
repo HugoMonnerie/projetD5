6bb31598b4fac25e70548e89c0c1ab7324e6f5e3
houssam <hlaghzil@gmail.com>
2019-12-11 15:17:57 +0100

    connect to data base

--- src/main/java/fr/projet5/App.java
+++ src/main/java/fr/projet5/App.java
@@ -1,17 +1,38 @@
 package fr.projet5;
 
 import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class App {
 
     public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
         Panneau panneau = new Panneau();
+        ResultSet resultSet = null;
+
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.43.223:3306/projet5", "hugo", "coding");
+            String connectionUrl =
+                    "jdbc:mysql://192.168.43.223:3306/projet5";
+            Connection connection = DriverManager.getConnection(connectionUrl, "hugo", "coding");
             String co = "Connected !";
+            String insertSql = "SELECT * FROM `matchs_tennis`";
+            PreparedStatement prepsInsertProduct = connection.prepareStatement(insertSql, Statement.RETURN_GENERATED_KEYS); {
+
+                prepsInsertProduct.execute();
+                // Retrieve the generated key from the insert.
+                resultSet = prepsInsertProduct.getGeneratedKeys();
+
+                // Print the ID of the inserted row.
+                while (resultSet.next()) {
+                    System.out.println("Generated: " + resultSet.getString(1));
+                }
             System.out.println(co);
-        } catch (SQLException e) {
+        }
+    } catch (SQLException e) {
             e.printStackTrace();
         }
     }
--- src/main/java/fr/projet5/Panneau.java
+++ src/main/java/fr/projet5/Panneau.java
@@ -12,7 +12,7 @@ public class Panneau extends JPanel {
         try {
             Image img = ImageIO.read(new File("src/main/resources/image.jpg"));
             Image img2 = ImageIO.read(new File("src/main/resources/image2.jpg"));
-            g.drawImage(img2, 0, 0, this.getWidth(), this.getHeight(), this);
+            //g.drawImage(img2, 0, 0, this.getWidth(), this.getHeight(), this);
         } catch (IOException e) {
             e.printStackTrace();
         }
