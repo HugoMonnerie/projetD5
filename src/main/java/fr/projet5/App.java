28ded2fd9c836139f84bc224e99cd2572b11e606
houssam <hlaghzil@gmail.com>
2019-12-11 13:56:31 +0100

    sql class

--- src/main/java/fr/projet5/App.java
+++ src/main/java/fr/projet5/App.java
@@ -1,21 +1,20 @@
 package fr.projet5;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+
+import static java.sql.DriverManager.getConnection;
+import static java.sql.DriverManager.println;
 
 public class App {
 
     public static void main(String[] args) {
         Fenetre fenetre = new Fenetre();
         Panneau panneau = new Panneau();
-        try {
-            Connection connection = DriverManager.getConnection("http://192.168.43.223/phpmyadmin/sql.php?server=1&db=projet5&table=football_team&pos=0&token=dd8e19cceb4659e5ced59a2caebbc38c", "hugo", "coding");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        SQLDatabaseConnection sqlDatabaseConnection = new SQLDatabaseConnection();
+
+        System.out.println("vv");
         String request = "SELECT * FROM players_tennis";
-        DriverManager.println(request);
+        println(request);
         System.out.println(request);
     }
 }
