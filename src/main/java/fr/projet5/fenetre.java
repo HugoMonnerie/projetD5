8795ba74358ca471550cfe3ce56043c297e4029a
houssam <hlaghzil@gmail.com>
2020-01-15 21:23:31 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/fr/projet5/fenetre.java
+++ src/main/java/fr/projet5/fenetre.java
@@ -1,15 +1,19 @@
 package fr.projet5;
+
 import javax.swing.*;
 import java.awt.*;
-public class Fenetre extends JFrame {
-    public Fenetre() {
+
+public class fenetre extends JFrame {
+
+    public fenetre(){
         this.setTitle("MySQL");
         this.setSize(1100, 600);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Panneau panneau = new Panneau();
-        Fenetre fen = this;
+        fenetre fen = this;
         this.setContentPane(panneau.panelConnection(fen));
+
         //Le conteneur principal
         JPanel content = new JPanel();
         content.setPreferredSize(new Dimension(300, 120));
@@ -17,6 +21,10 @@ public class Fenetre extends JFrame {
         content.setLayout(new GridBagLayout());
         //L'objet servant à positionner les composants
         new GridBagConstraints();
+
         this.setVisible(true);
     }
+
+
+
 }
